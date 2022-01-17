14385c0912bd9925cc14c9c2eccf76e4bce07cc0
WebFormP <pierre.sandron@cf2m.be>
2022-01-17 12:07:55 +0100

    adaptation du titre selon la page

--- public/header.php
+++ public/header.php
@@ -1,7 +1,27 @@
 <!DOCTYPE html>
 <html lang="fr">
 	<head>
-		<meta charset="utf-8" />
+
+        <!-- Comme le titre de la page est chaque fois différent,
+        il est nécessaire de connaître le nom du fichier demandé pour
+        savoir quel texte afficher dans la balise TITLE.
+        La variable $_SERVER['PHP_SELF'] permet de connaître le fichier lu actuellement
+        La fonction PHP basename() permet d'extraire le nom du fichier
+        -->
+        <?php
+            $title="";  /* cette variable contient le texte à placer dans <title> */
+            $idbody=""; /* cette variable contient l'id à mettre dans <body> pour le CSS */
+            switch(basename($_SERVER['PHP_SELF'])) {
+                case "homepage.php" : $title = "NewsWeb - Accueil"; $idbody = "home"; break;
+                case "blog.php" : $title = "NewsWeb - Tous les articles"; $idbody = "blog"; break;
+                case "article.php" : $title = "NewsWeb - Article"; $idbody = "blog"; break;
+                case "contact.php" : $title = "NewsWeb - Contact"; $idbody = "contact"; break;
+                default : $title = "NewsWeb";
+            }
+            echo "<title>".$title."</title>";
+        ?>
+
+        <meta charset="utf-8" />
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="stylesheet" href="css/style.css" />
@@ -19,13 +39,12 @@
 			<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 		<![endif]-->
 
-		<title>NewsWeb - Accueil</title>
-		
 		<!-- Librairie d'icônes -->
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	</head>
 
-	<body id="home">
+
+	<body id="<?php echo $idbody; ?>">
 		<div id="wrapper">
 			<!--__--__--__--__--__--  L O G O   --__--___--__--__--__-->
 			<div id="logo">
@@ -43,7 +62,7 @@
 					</li>
 					<li><a href="homepage.php" id="homenav">Accueil</a></li>
 					<li><a href="blog.php" id="blognav">Les articles</a></li>
-					<li><a href="contact.php" id="contact">Contact</a></li>
+					<li><a href="contact.php" id="contactnav">Contact</a></li>
 					<li><a href="admin.php" id="admin">Admin</a></li>
 				</ul>
 			</nav>
