db0fa7cc91d9711ced6934f862466db92523e359
WebFormP <pierre.sandron@cf2m.be>
2022-01-17 11:17:01 +0100

    pied de page commun - footer.php

---
+++ public/footer.php
@@ -0,0 +1,20 @@
+			<!--__--__--__--__--  T H E    F O O T E R --__--__--__--___--__--__--__-->
+			<footer>
+				<p>Design modifié pour un exercice de mise en page HTML/CSS</p>
+				<p>Copyright &copy 2013 Source X by Youssef Nassim and Zakariya Lhamri, design from <a href="http://geeksband.com">Geeksband.com</a>. All Rights Reserved.</p>
+			</footer>
+		</div>
+		
+		<!-- Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon -->
+		<script>
+		function hamburger() {
+			var x = document.getElementById("topnav");
+			if (x.className === "topnav") {
+				x.className += " responsive";
+			} else {
+				x.className = "topnav";
+			}
+		}
+		</script>
+	</body>
+</html>   
